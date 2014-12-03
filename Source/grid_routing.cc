c51247765a4449ff220fe1c49daa6ca65aa3dbbe
pren001 <pii199095@gmail.com>
2014-12-02 21:10:09 -0800

    Create grid_routing.cc

---
+++ Source/grid_routing.cc
@@ -0,0 +1,33 @@
+#include "../Headers/grid.h"
+#include "../Headers/edge.h"
+#include "../Headers/claim.h"
+#include "../Headers/problem_object.h"
+#include <math.h>
+
+using Utilities::map_Lee;
+using Utilities::map_Rubin;
+
+Utilities::grid_routing::grid_routing(ProblemObject* problem_object)
+{
+        this ->problem_object = problem_object;
+}
+
+Utilities::grid_routing::~grid_routing(){
+}
+
+
+Utilities::grid_routing::Lee_algorithm1();              /* no intersections */
+{
+        map_Lee *map_Lee = new map_Lee(problem_object);
+        paths = map_Lee -> Lee1();
+        map_Lee ->print();
+}
+
+Utilities::grid_routing::Lee_algorithm2();
+{
+        map_Lee *map_Lee = new map_Lee(problem_object);
+        paths = map_Lee -> Lee2();
+        map_Lee ->print();
+}
+
+Utilities::grid_routing::Rubin_algorithm1()
