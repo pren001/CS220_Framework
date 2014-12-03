ad744c5efd67da46b46cb5b08fb936c0564f77b1
pren001 <pii199095@gmail.com>
2014-12-03 15:36:20 -0800

    Update grid_routing.cc

--- Source/grid_routing.cc
+++ Source/grid_routing.cc
@@ -7,27 +7,46 @@
 using Utilities::map_Lee;
 using Utilities::map_Rubin;
 
-Utilities::grid_routing::grid_routing(ProblemObject* problem_object)
+void Utilities::grid_routing::grid_routing(ProblemObject* problem_object)
 {
         this ->problem_object = problem_object;
 }
 
-Utilities::grid_routing::~grid_routing(){
+void Utilities::grid_routing::~grid_routing(){
 }
 
 
-Utilities::grid_routing::Lee_algorithm1()             /* no intersections */
+void Utilities::grid_routing::Lee_algorithm1()             /* no intersections */
 {
         map_Lee *map_Lee = new map_Lee(problem_object);
         paths = map_Lee -> Lee1();
         map_Lee ->print();
 }
 
-Utilities::grid_routing::Lee_algorithm2()        /* unlimited intersections */
+void Utilities::grid_routing::Lee_algorithm2()        /* unlimited intersections */
 {
         map_Lee *map_Lee = new map_Lee(problem_object);
         paths = map_Lee -> Lee2();
         map_Lee ->print();
 }
 
-Utilities::grid_routing::Rubin_algorithm1()
+void Utilities::grid_routing::Rubin_algorithm1()
+{
+}
+
+void Utilities::gird_routing::print()
+{
+        if(paths.empty())
+        {
+                std::cout << "There is no path."
+                return;
+        }
+        for(unsigned i = 0; i < paths.size(); i++) {
+		cout << "Path " << i << ": ";
+		paths.at(i)->print();
+		Path* temp = paths.at(i);
+		delete temp;
+	}
+	paths.clear();
+}
+
